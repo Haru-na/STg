86d207bcaf455c1a7a7a5d967888ed55c00825ee
西川勇気 <sussy168168@gmail.com>
2021-06-21 12:59:43 +0900

    header変更 contact.page作成

---
+++ contact.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Contact</title>
+</head>
+<body>
+  <!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.gstatic.com">
+<link href="https://fonts.googleapis.com/css2?family=Stint+Ultra+Condensed&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="./assets/css/style.css">
+  <title>Contact</title>
+</head>
+<body>
+  <main class="normal">
+    <div class="contact normal">
+      <h2 class="normal">Contact</h2>
+        <form action="check.php" method="POST">
+          <div class="contact-box normal">
+            <input class="contacts" type="text" name="name" placeholder="名前">
+            <input class="contacts" type="email" name="email" placeholder="メール">
+            <textarea class="contacts" name="contact"  cols="30" rows="10" placeholder="お問い合わせ"></textarea>
+            <input class="submit" type="submit" value="送信">
+           </div>
+        </form>
+    </div>
+  </main>
+</body>
+</html>
+</body>
+</html>
