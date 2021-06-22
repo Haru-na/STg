f8bf2ac4a319d4e335fe1fef95b10d4929ef3a95
西川勇気 <sussy168168@gmail.com>
2021-06-22 20:11:38 +0900

    toppageに行くやつ追加

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,24 @@
 $(function() {
+  let wh = $(window).height();
+  let toppage = $('.toppage').offset().top;
+
   $('.btn-trigger').on('click', function() {
     $(this).toggleClass('active');
     $('.menu').toggleClass('is-active');
   });
+
+
+  $(window).scroll(function() {
+    if (toppage <= $(window).scrollTop() + wh) {
+      $('.toppage').fadeIn(10);
+    } else {
+      $('.toppage').hide();
+    }
+    $('.toppage').on('click', function(){
+      $('html,body').animate({
+          scrollTop: 0
+      }, '5000');
+      return false;
+  })
+  })
 });
